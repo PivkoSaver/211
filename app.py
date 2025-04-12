613826fd4a80874c7df3ecb05baa6706aa815ef8
PivkoSaver <pivko_saver@internet.ru>
2025-04-12 10:07:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,4 @@ def handleMessage(msg):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app)
-
-if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=10000, debug=True)
