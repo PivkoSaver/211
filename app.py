b8626a9d5f9dfe2b7ce64524e12fd8cc3e70c9db
PivkoSaver <pivko_saver@internet.ru>
2025-04-12 09:43:25 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_socketio import SocketIO, send
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['username'] = username
+            return redirect('/chat')
+        return 'Invalid credentials'
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+        if User.query.filter_by(username=username).first():
+            return 'User already exists'
+        db.session.add(User(username=username, password=password))
+        db.session.commit()
+        return redirect('/')
+    return render_template('register.html')
+
+@app.route('/chat')
+def chat():
+    if 'username' not in session:
+        return redirect('/')
+    return render_template('chat.html', username=session['username'])
+
+@socketio.on('message')
+def handleMessage(msg):
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    socketio.run(app)
