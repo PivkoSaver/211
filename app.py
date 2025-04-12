360020f536d05edb89d748a4bcb3f6a573a3e4b9
PivkoSaver <pivko_saver@internet.ru>
2025-04-12 10:03:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -55,3 +55,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     socketio.run(app)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=10000, debug=True)
